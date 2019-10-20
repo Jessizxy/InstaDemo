939ed5c0a5535de2276972871b8fb50215d6763f
Jessizxy <xz1315@nyu.edu>
2019-10-19 22:12:32 -0400

    able to show pics stored in database

--- Insta/urls.py
+++ Insta/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from Insta.views import HelloWorld
+from Insta.views import HelloWorld, PostsView, PostDetailView
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='home')
+    path('', HelloWorld.as_view(), name='home'),
+    path('posts/', PostsView.as_view(), name='posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 ]
